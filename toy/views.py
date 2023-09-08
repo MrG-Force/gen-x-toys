3fbe58d6fdeb133a0031cb32d7d75f7b44de3b93
MrGForce <willortizloza@hotmail.com>
2023-09-08 23:09:23 +1000

    Add toy detail page

--- toy/views.py
+++ toy/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Toy
+
+def detail(request, pk):
+    toy = get_object_or_404(Toy, pk=pk)
+    related_toys = Toy.objects.filter(category=toy.category, stock__gt=0).exclude(pk=pk)[0:3]
+    
+    return render(request, 'toy/detail.html', {
+        'toy': toy,
+        'related_toys': related_toys,
+    })
