8a4c4c5df3c805ada3624858d73ec5ed32c6c3d7
MrGForce <willortizloza@hotmail.com>
2023-09-11 13:48:00 +1000

    Add signup page

---
+++ core/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from random import choice, randint
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from faker import Faker
+
+fake = Faker()
+
+def generate_username_sample():
+    prefix = fake.prefix_nonbinary().replace('.', '_')
+    first_name = fake.first_name_nonbinary()
+    color = fake.color_name()
+    random_letter = fake.random_letter()
+    random_number = fake.random_number(2)
+
+    return f"{prefix}{first_name}_{color}_{random_letter}{random_number}"
+
+class SignupForm(UserCreationForm):
+
+    class Meta:
+        model = User
+        fields = ('username', 'accept_suggested_username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    INPUT_STYLES = 'w-full py-4 px-6 rounded-lg'
+
+
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        self.suggested_username = generate_username_sample()
+        self.fields['username'].widget.attrs.update({
+            'placeholder': f'Choose a cool username, like {self.suggested_username}',
+            'class': 'w-full py-4 px-6 rounded-lg'
+        })  
+    username = forms.CharField(max_length=30, help_text='Required', widget=forms.TextInput(attrs={
+        'class': INPUT_STYLES}))
+    accept_suggested_username = forms.BooleanField(required=False, initial=False, widget=forms.CheckboxInput(attrs={
+        'id': 'accept-suggested',
+        'label': 'Accept suggested username: ',
+        'onclick': 'acceptSuggestedName()'}))
+    email = forms.EmailField(max_length=254, help_text='Required', widget=forms.EmailInput(attrs={
+        'placeholder': 'Where can we spam you?',
+        'class': INPUT_STYLES}))
+    first_name = forms.CharField(max_length=50, help_text='Required', widget=forms.TextInput(attrs={
+        'placeholder': 'What do your friends yell when they see you?',
+        'class': INPUT_STYLES}))
+    last_name = forms.CharField(max_length=50, help_text='Required', widget=forms.TextInput(attrs={
+        'placeholder': 'You know, the name you hide when you\'re famous.',
+        'class': INPUT_STYLES}))
+    password1 = forms.CharField(help_text='Required', label='Password', widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your secret handshake in text form.',
+        'class': INPUT_STYLES}))
+    password2 = forms.CharField(help_text='Required', label='Repeat password', widget=forms.PasswordInput(attrs={
+        'placeholder': 'You sure? Type that secret handshake again.',
+        'class': INPUT_STYLES}))
--- core/views.py
+++ core/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from random import sample
 
 from toy.models import Category, Toy
 
+from .forms import SignupForm
+
 
 def index(request):
-    toys = Toy.objects.filter(stock__gt=0).order_by('-date_time_created')[0:3]
+    toys = Toy.objects.filter(stock__gt=0)
+    count = toys.count()
+    random_toys_indices = sample(range(count), min(3, count))
+    toys = [toys[i] for i in random_toys_indices]
+
     categories = Category.objects.all()
     
     return render(request, 'core/index.html', {
@@ -14,3 +21,18 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    suggested_username = form.suggested_username
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/login/')
+
+    return render(request, 'core/signup.html', {
+        'form': form,
+        'suggested_username': suggested_username,
+    })
